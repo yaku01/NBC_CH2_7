ccaa142f8d3bddb4f0d28e835722f72111d9d1c7
Cha0910 <ae20414@naver.com>
2026-03-31 21:13:42 +0900

    fix: 방어구 교체 시 체력 손실 버그 수정 - 방어구 교체 전 체력을 저장한 후 저장한 체력이 교체 후 체력보다 높으면 체력을 조정 - 교체 전 체력이 현재 최대 체력 보다 높을 경우 최대 체력으로 설정

--- Project1/Characters/Character.cpp
+++ Project1/Characters/Character.cpp
@@ -247,7 +247,10 @@ std::unique_ptr<IItem> Character::EquipArmor(std::unique_ptr<IItem> new_armor)
 		std::cout << "이 아이템은 방어구가 아닙니다." << std::endl;
 		return new_armor; // 잘못된 아이템 반환
 	}
-	
+
+    // 방어구 장착 전 체력 저장
+    int prev_health = health;
+  
 	// 장착한 방어구가 존재하면
 	std::unique_ptr<IItem> old_armor = UnequipArmor(); // 기존 방어구 제거
 
@@ -258,6 +261,12 @@ std::unique_ptr<IItem> Character::EquipArmor(std::unique_ptr<IItem> new_armor)
 		auto* armor = static_cast<EquippableItem*>(equipped_armor.get());
 		armor->Equip(*this); // 새 방어구 장착
 	}
+
+    // 방어구 장착으로 최대 체력이 감소했을 때, 현재 체력을 최대 체력에 맞게 조정
+    if(prev_health > health)
+    {
+        health = (prev_health > max_health) ? max_health : prev_health;
+    }
 
 	return old_armor; // 이전에 장착했던 방어구 반환
 }
